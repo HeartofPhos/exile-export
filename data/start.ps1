e9a2c0e86fb80b5960728102c9c6c5459b49b125
heart <>
2022-07-08 09:26:03 +0200

    split and min json

--- data/start.ps1
+++ data/start.ps1
@@ -9,6 +9,7 @@ DepotDownloader `
 
 New-Item -ItemType Directory -Force -Path ./out_dir
 New-Item -ItemType Directory -Force -Path ./temp_dir
+New-Item -ItemType Directory -Force -Path ./split
 
 pypoe_exporter config set out_dir ./out_dir
 pypoe_exporter config set temp_dir ./temp_dir
@@ -16,5 +17,9 @@ pypoe_exporter config set ggpk_path ./game
 pypoe_exporter setup perform
 
 $dat_list = Get-Content ./dat-list.txt
-
 pypoe_exporter dat json ./out.json --files $dat_list
+
+$out_json = Get-Content ./out.json -raw | ConvertFrom-Json
+foreach ($dat in $out_json) {
+    $dat | ConvertTo-Json -Compress | Out-File -FilePath "./split/$($dat.filename).json"
+}
