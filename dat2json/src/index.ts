b2ff21d93b02367806aa42bcd273aebbf55d1ee3
heart <>
2022-07-10 00:20:57 +0200

    better error handling

--- dat2json/src/index.ts
+++ dat2json/src/index.ts
@@ -34,46 +34,55 @@ async function main() {
 
     const schema = await getSchema();
     for (const file of extractedDatList) {
-      const { base, name } = path.parse(file);
+      try {
+        const { base, name } = path.parse(file);
 
-      console.log(base);
+        console.log(base);
 
-      const tableName = name;
-      const datBuffer = await fs.promises.readFile(file);
+        const tableName = name;
+        const datBuffer = await fs.promises.readFile(file);
 
-      const datFile = readDatFile(base, datBuffer);
-      const schemaLookup = buildSchemaLookup(schema, tableName, datFile);
+        const datFile = readDatFile(base, datBuffer);
+        const schemaLookup = buildSchemaLookup(schema, tableName, datFile);
 
-      const output: Output = {
-        columns: [],
-        data: Array(datFile.rowCount),
-      };
+        const output: Output = {
+          columns: [],
+          data: Array(datFile.rowCount),
+        };
 
-      for (let i = 0; i < datFile.rowCount; i++) {
-        output.data[i] = {};
-      }
-
-      let unknownCounter = 0;
-      for (const key in schemaLookup) {
-        const { column, header } = schemaLookup[key];
-        
-        const name = column.name || `Unknown_${unknownCounter++}`
-        output.columns.push({
-          name: name,
-          type: column.type,
-          references: column.references?.table || null,
-        });
-
-        const columns = readColumn(header, datFile);
         for (let i = 0; i < datFile.rowCount; i++) {
-          output.data[i][name] = columns[i];
+          output.data[i] = {};
         }
-      }
 
-      await fs.promises.writeFile(
-        `${outputDir}/${tableName}.dat.json`,
-        JSON.stringify(output, undefined, 2)
-      );
+        let unknownCounter = 0;
+        for (const key in schemaLookup) {
+          const { column, header } = schemaLookup[key];
+
+          if (!column) {
+            console.log(`Missing schema data, Table ${file}, Header ${key}`);
+            continue;
+          }
+
+          const name = column.name || `Unknown_${unknownCounter++}`;
+          output.columns.push({
+            name: name,
+            type: column.type,
+            references: column.references?.table || null,
+          });
+
+          const columns = readColumn(header, datFile);
+          for (let i = 0; i < datFile.rowCount; i++) {
+            output.data[i][name] = columns[i];
+          }
+        }
+
+        await fs.promises.writeFile(
+          `${outputDir}/${tableName}.dat.json`,
+          JSON.stringify(output, undefined, 2)
+        );
+      } catch (e) {
+        console.log(`Something went wrong while parsing ${file}, ${e}`);
+      }
     }
   } finally {
     await fs.promises.rmdir(tempDir, { recursive: true });
